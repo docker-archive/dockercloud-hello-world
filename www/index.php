e2061cae1c22f2739d7f784d0188b26012188a2d
Saif Eddin Gmati <azjezz@azjezz.me>
2018-08-21 03:59:36 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -22,24 +22,23 @@
 	<?php
 	
     $links = [];
-	foreach($_ENV as $key => $value) {
+    foreach($_ENV as $key => $value) {
         if(preg_match('/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/', $key, $matches)) {
-    	    $links[] = [
-			    'name' => $matches[1],
+    	   $links[] = [
+		'name' => $matches[1],
                 'port' => $matches[2],
-			    'proto' => $matches[3],
-			    'value' => $value
-		    ];
-	    }
+		'proto' => $matches[3],
+	    	'value' => $value
+	   ];
+	}
     }
 
-    if(0 !== count($links)) : 
-    ?>
-		<h3>Links found</h3>
-		<?php foreach($links as $link) : ?>
-			<b><?= $link["name"]; ?></b> listening in <?= $link["port"]+"/"+$link["proto"]; ?> available at <?= $link["value"]; ?><br />
-	    <?php endforeach; ?>
-	<?php endif; ?>
+    if(0 !== count($links)) : ?>
+	<h3>Links found</h3>
+	<?php foreach($links as $link) : ?>
+	    <b><?= $link["name"]; ?></b> listening in <?= $link["port"]+"/"+$link["proto"]; ?> available at <?= $link["value"]; ?><br />
+    	<?php endforeach; ?>
+    <?php endif; ?>
     <?= (false !== getenv('DOCKERCLOUD_AUTH')) ? '<h3>I have Docker Cloud API powers!</h3>' : ''; ?>
 </body>
 </html>
